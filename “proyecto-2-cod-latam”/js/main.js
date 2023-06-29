ab57a68226a2d69d7be68d032a49d074d627d957
maurennys4 <133044989+maurennys4@users.noreply.github.com>
2023-06-29 11:32:20 -0500

    Add files via upload

---
+++ “proyecto-2-cod-latam”/js/main.js
@@ -0,0 +1,15 @@
+let.addEventListener("load",function(){
+document.getElementById("cambiarColor").addEventListener("click",function(){       
+var colorFondo = document.addEventListener("colorFondo").value;
+document.addEventListener("cuerpo").setAtribute(bgcolor,colorFondo)
+    })
+
+   }) 
+
+    
+
+    
+
+
+
+       
